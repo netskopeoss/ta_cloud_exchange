e7bf2c765f45704c7441eb3159a67f9759392dfe
satyarajsinh_crest <satyarajsinh.jadeja@crestdatasys.com>
2024-02-15 14:54:03 +0530

    Release 5.0.1 Beta

--- data/database-init/mongo-init.sh
+++ data/database-init/mongo-init.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-mongo -- "$MONGO_INITDB_DATABASE" <<EOF
+mongosh -- "$MONGO_INITDB_DATABASE" <<EOF
     var user = '$MONGODB_USERNAME';
     var passwd = '$MONGODB_PASSWORD';
     var dbName = '$MONGO_INITDB_DATABASE';
